d945226d28126280eb36dd753d4d9f6e675637cb
DaZuo0122 <76245335+DaZuo0122@users.noreply.github.com>
2025-10-21 17:09:00 +0800

    Add files via upload

--- binaries/netwarden-cli/src/main.rs
+++ binaries/netwarden-cli/src/main.rs
@@ -1,6 +1,14 @@
-use netinfo::{print_interfaces, print_ports};
+use anyhow::Result;
+use netinfo::{PortSet, SensitiveSocket, list_sensitve_sockets, print_interfaces, print_ports};
 
-fn main() {
+fn main() -> Result<()> {
     print_interfaces();
-    print_ports();
+    // print_ports();
+    println!("Start constructing PortSet");
+    let port_set = PortSet::default();
+    let ss_list = list_sensitve_sockets(&port_set)?;
+    for s_socket in ss_list {
+        println!("Found sensitive socket: {:?}", s_socket);
+    }
+    Ok(())
 }
--- libraries/netinfo/src/lib.rs
+++ libraries/netinfo/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Ok;
 use anyhow::{Result, anyhow};
 use local_ip_address::list_afinet_netifas;
 use netstat2::{
@@ -5,9 +6,16 @@ use netstat2::{
 };
 use std::collections::BTreeSet;
 use std::fmt;
+use std::fs;
 use std::net::IpAddr;
+use std::path::Path;
+use std::path::PathBuf;
 use std::str::FromStr;
 
+#[cfg(target_os = "windows")]
+use dunce;
+
+#[derive(Debug, Clone)]
 pub struct SensitiveSocket {
     pub address_type: AddrType,
     pub port: u16,
@@ -17,22 +25,72 @@ pub struct SensitiveSocket {
     pub local_addr: String,
 }
 
+#[derive(Debug, Clone)]
 pub enum AddrType {
     IPv4,
     IPv6,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum L4Protocol {
     TCP,
     UDP,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct PortSet {
     // keep separate sets for TCP and UDP ports
     tcp: BTreeSet<u16>,
     udp: BTreeSet<u16>,
+}
+
+pub fn list_sensitve_sockets(port_set: &PortSet) -> Result<Vec<SensitiveSocket>> {
+    let af = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+    let proto = ProtocolFlags::TCP | ProtocolFlags::UDP;
+    let sockets = get_sockets_info(af, proto)?;
+
+    let mut sockets_list = Vec::new();
+    for si in sockets {
+        match si.protocol_socket_info {
+            ProtocolSocketInfo::Tcp(tcp) => {
+                if is_any_addr(&tcp.local_addr)
+                    && port_set.contains(&L4Protocol::TCP, tcp.local_port)
+                {
+                    let addr_type = match tcp.local_addr {
+                        IpAddr::V4(_) => AddrType::IPv4,
+                        IpAddr::V6(_) => AddrType::IPv6,
+                    };
+                    sockets_list.push(SensitiveSocket {
+                        address_type: addr_type,
+                        port: tcp.local_port,
+                        pids: si.associated_pids,
+                        protocol: L4Protocol::TCP,
+                        state: Some(map_tcp_state(tcp.state)),
+                        local_addr: addr_to_string(&tcp.local_addr),
+                    });
+                }
+            }
+            ProtocolSocketInfo::Udp(udp) => {
+                if is_any_addr(&udp.local_addr)
+                    && port_set.contains(&L4Protocol::UDP, udp.local_port)
+                {
+                    let addr_type = match udp.local_addr {
+                        IpAddr::V4(_) => AddrType::IPv4,
+                        IpAddr::V6(_) => AddrType::IPv6,
+                    };
+                    sockets_list.push(SensitiveSocket {
+                        address_type: addr_type,
+                        port: udp.local_port,
+                        pids: si.associated_pids,
+                        protocol: L4Protocol::UDP,
+                        state: None,
+                        local_addr: addr_to_string(&udp.local_addr),
+                    });
+                }
+            }
+        }
+    }
+    Ok(sockets_list)
 }
 
 impl PortSet {
@@ -42,6 +100,109 @@ impl PortSet {
             L4Protocol::UDP => self.udp.contains(&port),
         }
     }
+}
+
+impl Default for PortSet {
+    fn default() -> Self {
+        let mut port_set = PortSet {
+            tcp: BTreeSet::new(),
+            udp: BTreeSet::new(),
+        };
+        let possible_paths = gen_default_path().unwrap();
+        for path in &possible_paths {
+            if path.exists() {
+                // Load ports from the first existing file found
+                if load_ports_from_file(path, &mut port_set).is_ok() {
+                    break; // Exit after successfully loading from the first existing file
+                }
+            }
+        }
+        port_set // Return the port_set after attempting to load from files
+    }
+}
+
+fn gen_default_path() -> Result<[PathBuf; 3]> {
+    let exec_dir = std::env::current_exe()
+        .ok()
+        .and_then(|path| path.parent().map(|p| p.to_path_buf()))
+        .unwrap_or(std::env::current_dir().unwrap_or_default());
+
+    // Use dunce for path canonicalization on Windows to handle junctions and symlinks properly
+    #[cfg(target_os = "windows")]
+    let possible_paths = [
+        dunce::canonicalize(exec_dir.join("data").join("sensitive-ports.txt"))
+            .unwrap_or_else(|_| exec_dir.join("data").join("sensitive-ports.txt")), // For distribution
+        dunce::canonicalize(
+            std::env::current_dir()
+                .unwrap_or_default()
+                .join("data")
+                .join("sensitive-ports.txt"),
+        )
+        .unwrap_or_else(|_| {
+            std::env::current_dir()
+                .unwrap_or_default()
+                .join("data")
+                .join("sensitive-ports.txt")
+        }), // For development
+        dunce::canonicalize(Path::new("data").join("sensitive-ports.txt"))
+            .unwrap_or_else(|_| Path::new("data").join("sensitive-ports.txt")), // Relative path
+    ];
+
+    #[cfg(target_os = "linux")]
+    let possible_paths = [
+        exec_dir.join("data").join("sensitive-ports.txt"), // For distribution
+        std::env::current_dir()
+            .unwrap_or_default()
+            .join("data")
+            .join("sensitive-ports.txt"), // For development
+        Path::new("data").join("sensitive-ports.txt"),     // Relative path
+    ];
+
+    Ok(possible_paths)
+}
+
+fn load_ports_from_file<P: AsRef<Path>>(path: P, port_set: &mut PortSet) -> Result<()> {
+    let content = fs::read_to_string(path)?;
+
+    for line in content.lines() {
+        let line = line.trim();
+        if line.is_empty() || line.starts_with('#') {
+            continue;
+        }
+
+        if let Some((port_range, protocol_str)) = line.rsplit_once('/') {
+            let protocol = L4Protocol::from_str(protocol_str)?;
+
+            // Handle both single ports (e.g., "80") and port ranges (e.g., "40000-50000")
+            if let Some((start, end)) = port_range.split_once('-') {
+                let start_port: u16 = start.trim().parse()?;
+                let end_port: u16 = end.trim().parse()?;
+
+                for port in start_port..=end_port {
+                    match protocol {
+                        L4Protocol::TCP => {
+                            port_set.tcp.insert(port);
+                        }
+                        L4Protocol::UDP => {
+                            port_set.udp.insert(port);
+                        }
+                    }
+                }
+            } else {
+                let port: u16 = port_range.parse()?;
+                match protocol {
+                    L4Protocol::TCP => {
+                        port_set.tcp.insert(port);
+                    }
+                    L4Protocol::UDP => {
+                        port_set.udp.insert(port);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
 /// Redefined TcpState in netstat2 to avoid introducing it multiple times
@@ -133,6 +294,24 @@ pub fn map_tcp_state(s: TcpState) -> TState {
         N::TimeWait => TState::TimeWait,
         N::DeleteTcb => TState::DeleteTcb,
         _ => TState::Unknown,
+    }
+}
+
+fn is_any_addr(addr: &IpAddr) -> bool {
+    match addr {
+        IpAddr::V4(v4) => v4.octets() == [0, 0, 0, 0],
+        IpAddr::V6(v6) => v6.is_unspecified(),
+    }
+}
+
+fn addr_to_string(addr: &IpAddr) -> String {
+    if addr.is_unspecified() {
+        match addr {
+            IpAddr::V4(_) => "0.0.0.0".into(),
+            IpAddr::V6(_) => "::".into(),
+        }
+    } else {
+        addr.to_string()
     }
 }
 
